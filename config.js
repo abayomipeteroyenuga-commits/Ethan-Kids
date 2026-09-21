451316306bf3e5edd4a7ddd87389b540cb055ce0
abayomipeteroyenuga-commits <abayomipeteroyenuga@gmail.com>
2026-09-21 23:57:43 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Public Ethan ID Supabase configuration; never include service-role keys.
-window.ETHAN_KIDS_CONFIG={supabaseUrl:'https://hrymakhatxjitbgpdbjk.supabase.co',supabasePublishableKey:'sb_publishable_lZk0iM8AuN1ITiclDFCmZA_Sn35Fek0',ssoFunctionUrl:'https://hrymakhatxjitbgpdbjk.supabase.co/functions/v1/ethan-sso',hubUrl:'https://hub.ethandigitalacademy.org'};
+window.ETHAN_KIDS_CONFIG={supabaseUrl:'https://hrymakhatxjitbgpdbjk.supabase.co',supabasePublishableKey:'sb_publishable_lZk0iM8AuN1ITiclDFCmZA_Sn35Fek0',ssoFunctionUrl:'https://hrymakhatxjitbgpdbjk.supabase.co/functions/v1/ethan-sso-kids',hubUrl:'https://hub.ethandigitalacademy.org'};
